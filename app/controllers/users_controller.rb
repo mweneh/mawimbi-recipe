368af4e5bcd890973ef75cdc9523032ed9e91ce2
David <davidongaroh@gmail.com>
2022-10-28 08:55:13 +0300

    edit skip before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, only: [:create, :destroy]
+    skip_before_action :authorize, only: [:create]
 
     def index
       render json: User.all, status: 200
