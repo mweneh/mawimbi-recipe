c631cdd3653df9875b0468a771ca6c8543d412e8
David <davidongaroh@gmail.com>
2022-10-28 09:01:41 +0300

    configure session store

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,15 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MawimbiRecipe
+module ReactRailsApiProjectTemplate
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    # Adding cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    # https://owasp.org/www-community/SameSite
+    config.action_dispatch.cookies_same_site_protection = :strict
 
     # Configuration for the application, engines, and railties goes here.
     #
