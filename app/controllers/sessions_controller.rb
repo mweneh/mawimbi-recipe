5c04d72442a87698753d273f4efbcd8a5581e04a
David <davidongaroh@gmail.com>
2022-10-28 08:51:34 +0300

    add login and logout to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+    skip_before_action :authorize, only: :login
+  
+    def login
+      user = User.find_by(username: params[:username])
+      if user&.authenticate(params[:password])
+        create_user_session(user.id, user.user_type)
+        render json: user
+      else
+        render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+      end
+    end
+  
+    def logout
+      delete_user_session
+      head :no_content
+    end
+
+    private
+
+    def create_user_session user_id, user_type
+        session[:user_id] ||= user_id
+        session[:user_type] ||= user_type
+    end
+
+    def delete_user_session
+        session.delete :user_id
+        session.delete :user_type
+    end
+  
 end
