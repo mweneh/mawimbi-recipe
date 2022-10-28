fd07f9c48cee20960c3491d341e551578548f4a1
David <davidongaroh@gmail.com>
2022-10-28 08:57:10 +0300

    authorize users and rescue errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
-end
+    include ActionController::Cookies
+  
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+    before_action :authorize
+  
+    private
+  
+    def authorize
+      @user = User.find_by(id: session[:user_id])
+  
+      render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+    end
+  
+    def authorize_admin
+      render json:{errors: "You cannot perform that action!"}, status: 401 unless session[:user_type] == "admin"
+  end
+  
+    def render_unprocessable_entity_response(exception)
+      render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+  
+    def render_not_found_response
+      render json: { errors: "Item not found" }, status: :not_found
+    end
+  end
